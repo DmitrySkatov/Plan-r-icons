b63cdcf9d012543cc87a93942361bc990cb4c142
Скатов Дмитрий Андреевич <SkatovDA@dpractices.local>
2022-06-16 16:10:54 +0300

    initial commit

---
+++ dev/serve.vue
@@ -0,0 +1,18 @@
+<script>
+import { defineComponent } from 'vue';
+// Uncomment import and local "components" registration if library is not registered globally.
+import { PlanrIconsSample } from '@/entry.esm';
+
+export default defineComponent({
+  name: 'ServeDev',
+  components: {
+   PlanrIconsSample,
+  }
+});
+</script>
+
+<template>
+  <div id="app">
+    <planr-icons-sample class-icon="Up" color="blue" font-size="14px" />
+  </div>
+</template>
---
+++ src/lib-components/planr-icons-sample.vue
@@ -0,0 +1,26 @@
+<template >
+       <span :class="'p-icon-'+ classIcon" :style="{fontSize: fontSize,color: color }"></span>
+      
+</template>
+
+
+<script>
+
+export default {
+    props: {
+        classIcon:{
+            type: String,
+        },
+        fontSize:{
+            type: String,
+        },
+        color:{
+            type: String,
+        }
+    }
+}
+</script>
+<style>
+@import '../plan-r.css'
+
+</style>
